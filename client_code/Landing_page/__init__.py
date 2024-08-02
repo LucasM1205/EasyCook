59bd5e7082b61c5f48624bf10b10e78724b539ea
anvil_acc@outlook.com <anvil_acc@outlook.com>
2024-08-02 04:17:46 +0000

    Created EasyCook_logo.png and Landing_page; Edited settings and theme/parameters.yaml; Deleted Form1

--- client_code/Landing_page/__init__.py
+++ client_code/Landing_page/__init__.py
@@ -1,8 +1,8 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Landing_pageTemplate
 from anvil import *
 
 
-class Form1(Form1Template):
+class Landing_page(Landing_pageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)

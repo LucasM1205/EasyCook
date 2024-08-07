b535cc1c576be365a0d4d6076566449aa6c2c6ac
anvil_acc@outlook.com <anvil_acc@outlook.com>
2024-08-07 13:25:32 +0000

    Edited theme/parameters.yaml, Main_page.Veggie_category.RowTemplate1 and Main_page.Veggie_category

--- client_code/Main_page/Veggie_category/RowTemplate1/__init__.py
+++ client_code/Main_page/Veggie_category/RowTemplate1/__init__.py
@@ -13,9 +13,8 @@ class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
 
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
     alert(content=f"Test")

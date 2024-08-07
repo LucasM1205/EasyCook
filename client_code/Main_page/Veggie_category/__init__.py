129acb3cc14fd758072a9235fee7bd11d0a0745d
anvil_acc@outlook.com <anvil_acc@outlook.com>
2024-08-07 08:55:30 +0000

    Created Main_page.Veggie_category.RecipeDetailForm.ItemTemplate4; Edited Main_page.Veggie_category and Main_page.Veggie_category.RecipeDetailForm

--- client_code/Main_page/Veggie_category/__init__.py
+++ client_code/Main_page/Veggie_category/__init__.py
@@ -20,3 +20,24 @@ class Veggie_category(Veggie_categoryTemplate):
     """This method is called when the link is clicked"""
     open_form('Main_page')
 
+  def get_ingredients_for_recipe(self, recipe_id):
+        recipe_ingredients = app_tables.recipe_ingredients.search(RecipeID=recipe_id)
+        ingredients = []
+        for item in recipe_ingredients:
+            ingredient = app_tables.ingredients.get(IngredientID=item['IngredientID'])
+            ingredients.append({
+                'name': ingredient['Name'],
+                'quantity': item['Quantity']
+            })
+        return ingredients
+
+  def show_recipe_detail(self, recipe):
+        recipe_detail_form = RecipeDetailForm()
+        recipe_detail_form.label_recipe_name.text = recipe['Name']
+        recipe_detail_form.repeating_panel_ingredients.items = self.get_ingredients_for_recipe(recipe['RecipeID'])
+        open_form(recipe_detail_form)
+
+  def repeating_panel_1_item_click(self, **event_args):
+        selected_recipe = self.repeating_panel_1.get_components()[event_args['item_index']]
+        self.show_recipe_detail(selected_recipe)
+

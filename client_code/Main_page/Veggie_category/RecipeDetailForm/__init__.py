a175da887ca3a77e333ec8ee3c11df5b166509a5
anvil_acc@outlook.com <anvil_acc@outlook.com>
2024-08-07 11:40:31 +0000

    Created Main_page.Veggie_category.RecipeDetailForm and Main_page.Veggie_category.RecipeDetailForm.ItemTemplate4; Edited Main_page.Veggie_category; Deleted Main_page.Veggie_category.RecipesDetailForm and Main_page.Veggie_category.RecipesDetailForm.ItemTemplate4

--- client_code/Main_page/Veggie_category/RecipeDetailForm/__init__.py
+++ client_code/Main_page/Veggie_category/RecipeDetailForm/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RecipesDetailFormTemplate
+from ._anvil_designer import RecipeDetailFormTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,9 +9,10 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class RecipesDetailForm(RecipesDetailFormTemplate):
+class RecipeDetailForm(RecipeDetailFormTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.repeating_panel_1.items = app_tables.recipes.search(Category='Veggie')
+
     # Any code you write here will run before the form opens.

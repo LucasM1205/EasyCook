287c636d9bf3e8f0ee2b39422a18d11073b48061
anvil_acc@outlook.com <anvil_acc@outlook.com>
2024-08-07 06:03:20 +0000

    Created Main_page.Veggie_Category and Main_page.Veggie_Category.ItemTemplate1

---
+++ client_code/Main_page/Veggie_Category/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import Veggie_CategoryTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class Veggie_Category(Veggie_CategoryTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.

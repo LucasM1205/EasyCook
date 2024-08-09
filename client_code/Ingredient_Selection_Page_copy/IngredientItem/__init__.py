8a4c8a0f29e15f784ae53490944bbea29e2c2aa3
anvil_acc@outlook.com <anvil_acc@outlook.com>
2024-08-09 06:31:32 +0000

    Edited Ingredient_Selection_Page_copy.IngredientItem and Ingredient_Selection_Page_copy

--- client_code/Ingredient_Selection_Page_copy/IngredientItem/__init__.py
+++ client_code/Ingredient_Selection_Page_copy/IngredientItem/__init__.py
@@ -20,4 +20,8 @@ class IngredientItem(IngredientItemTemplate):
   def check_box_1_change(self, **event_args):
     """This method is called when the check box is changed"""
     self.item['selected'] = self.check_box_1.checked
+    # Debugging-Ausgabe, um zu sehen, wann sich der Wert ändert
+    print(f"{self.item['ingredient']} selected: {self.item['selected']}")
+    #if hasattr(self.parent, 'update_selection'):
+      #self.parent.update_selection()
     # Any code you write here will run before the form opens.
--- client_code/Ingredient_Selection_Page_copy/__init__.py
+++ client_code/Ingredient_Selection_Page_copy/__init__.py
@@ -11,25 +11,32 @@ from anvil.tables import app_tables
 class Ingredient_Selection_Page_copy(Ingredient_Selection_Page_copyTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
-    
     # Load the ingredients from the server
     ingredients = anvil.server.call('get_all_ingredients')
-    
     # Debugging-Ausgabe
-    print("Ingredients loaded:", ingredients)
-    
+    #print("Ingredients loaded:", ingredients)
     # Set the repeating panel items
     self.repeating_panel_1.items = [{'ingredient': ingredient, 'selected': True} for ingredient in ingredients]
-
+    # Debugging-Ausgabe um zu überprüfen, ob alle Items korrekt geladen wurden
+    print(f"Ingredients loaded: {ingredients}")
+  
+  def update_selection(self):
+    # This method updates the selected ingredients based on the checkboxes
+    selected_ingredients = [item['ingredient'] for item in self.repeating_panel_1.items if item['selected']]
+    print("Currently selected ingredients:", selected_ingredients)
+  
   def link_1_click(self, **event_args):
     open_form("Main_page")
 
   def continue_click(self, **event_args):
+
+    for item in self.repeating_panel_1.items:
+      print(f"Item: {item['ingredient']}, Selected: {item['selected']}")
+    
     # Get selected ingredients
     selected_ingredients = [item['ingredient'] for item in self.repeating_panel_1.items if item['selected']]    
-    
     # Debugging-Ausgabe
-    print("Selected Ingredients:", selected_ingredients)
-    
+    #print("Selected Ingredients:", selected_ingredients)
+    print(f"Selected Ingredients before moving to next page: {selected_ingredients}")
     # Pass selected ingredients to the next page
     open_form('RecipeResultsPage', selected_ingredients=selected_ingredients)
